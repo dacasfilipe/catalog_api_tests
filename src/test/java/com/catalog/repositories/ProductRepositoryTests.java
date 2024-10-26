538206ad010727208b9655e4bb130af8de0119ae
dacasfilipe <filiperdacas@gmail.com>
2024-10-25 21:58:08 -0300

    feat: add unit tests in ProductRepositoryTests

--- src/test/java/com/catalog/repositories/ProductRepositoryTests.java
+++ src/test/java/com/catalog/repositories/ProductRepositoryTests.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.Instant;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 public class ProductRepositoryTests {
@@ -23,6 +26,21 @@ public class ProductRepositoryTests {
         Optional<Product> result = repository.findById(existingId);
         //testa se o objeto está presente
         Assertions.assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void testSaveProduct(){
+        Product product = new Product();
+        product.setName("Test Product");
+        product.setDescription("testando");
+        product.setPrice(5000.00);
+        product.setImgUrl("localhost/image");
+        //product.setDate(Instant.parse("2024-10-25T19:44:00.00+03:00"));
+        //salvar o produto
+        Product savedProduct = repository.save(product);
+        //Asserts são para certificar se tudo deu certo
+        assertThat(savedProduct).isNotNull();//verifica se é não nulo
+        assertThat(savedProduct.getName()).isEqualTo("Test Product");
     }
 
 
